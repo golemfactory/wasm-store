bc56c474235b05c20b14d6ff4d414161b9fe36c4
lukasz-glen <29129196+lukasz-glen@users.noreply.github.com>
2019-07-09 16:42:16 +0200

    Add files via upload

--- hello/hello.c
+++ hello/hello.c
@@ -4,6 +4,10 @@ int main(int argc, char** argv) {
   char* name = argc >= 2 ? argv[1] : "anonymous";
 
   FILE* f_out = fopen("out.txt", "w");
+  if (f_out == NULL) {
+    printf("couldn't open file for writing\n");
+    return 1;
+  }
 
   fprintf(f_out, "hello %s!\n", name);
 
